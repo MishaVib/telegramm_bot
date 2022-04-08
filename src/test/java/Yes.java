261803c93143c679f5e97f37b5b44975b86a7edc
Misha_Vib <nsutkov382@gmail.com>
2022-04-08 16:14:58 +0300

    add

---
+++ src/test/java/Yes.java
@@ -0,0 +1,10 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+public class Yes {
+    @Test
+     void firs() {
+
+        Selenide.open("https://www.ozon.ru/");
+    }
+}
